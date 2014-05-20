5fc083da4cd2e536e798e3d11e22450072c8d06f
Mohammad Abuelezz <m.abuelezz@yahoo-inc.com>
2014-05-20 12:23:24 +0300

    first commit

---
+++ functions/script.js
@@ -0,0 +1,12 @@
+function Animal (){
+  var name, age;
+  return console.dir(this);
+}
+
+hourse = new Animal;
+hourse.name = 'Heee';
+hourse.age =4;
+
+dog = new Animal();
+dog.name = '3aaw';
+dog.age = 3;
